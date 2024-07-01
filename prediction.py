f9f950a4c57dee43795e3adec2db94ddeeedf7be
Tesnime_Ellabou <101891297+tass25@users.noreply.github.com>
2024-07-01 15:53:14 +0200

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 # Create the dataset
 data = {
